9de059b3f0a94391137a624d8fc72f4b9b4ccba8
D shivaramkrishna <eng24ct0004@dsu.edu.in>
2025-08-06 18:17:09 +0530

    Create form.js

---
+++ form.js
@@ -0,0 +1,28 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyppeguwuE2Xb_lOINcYvXZwWMOl26MlE0qmBT9k8XKOqxwLdetB1zd46ERO_Q7qvhY-Q/exec';
+const form = document.getElementById('kcetForm');
+const responseMsg = document.getElementById('response');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const formData = {
+    name: form.name.value,
+    cet: form.cet.value,
+    choice: form.choice.value,
+    remarks: form.remarks.value
+  };
+
+  fetch(scriptURL, {
+    method: 'POST',
+    body: JSON.stringify(formData),
+    headers: { 'Content-Type': 'application/json' }
+  })
+  .then(res => {
+    responseMsg.innerText = "✅ Submission successful!";
+    form.reset();
+  })
+  .catch(err => {
+    responseMsg.innerText = "❌ Submission failed. Try again.";
+    console.error(err);
+  });
+});
